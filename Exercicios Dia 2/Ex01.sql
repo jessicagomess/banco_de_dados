4133ffdecda0f55614189cdfa749f2f877874489
Jessica Gomes <J.gomesss@outlook.com>
2025-01-08 20:48:08 -0300

    adicionando exercicios

---
+++ Exercicios Dia 2/Ex01.sql
@@ -0,0 +1,54 @@
+CREATE DATABASE db_generation_game_online;
+
+USE db_generation_game_online;
+
+CREATE TABLE tb_classes(
+	id BIGINT AUTO_INCREMENT PRIMARY KEY,
+	nome_classe VARCHAR(50) NOT NULL,
+    arma VARCHAR(100) NOT NULL
+);
+
+INSERT INTO tb_classes(nome_classe, arma)
+VALUES ("Arqueiro", "Arco"),
+	   ("Lanceiro", "Lança"),
+       ("Mago", "Catalisador"),
+       ("Espadachim", "Espada"),
+       ("Curandeiro", "Bastão");
+    
+CREATE TABLE tb_personagens(
+	id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    elemento VARCHAR(100) NOT NULL,
+    ataque INT,
+    defesa INT,
+    classes_id BIGINT,
+	FOREIGN KEY (classes_id) REFERENCES tb_classes(id)
+);
+
+INSERT INTO tb_personagens(nome, elemento, ataque, defesa, classes_id)
+VALUES ("Venti", "Vento", 100, 50, 1),
+	   ("Hu Tao", "Fogo", 120, 30, 2),
+       ("Mona", "Água", 95, 40, 3),
+       ("Raiden Shogun", "Eletricidade", 2500, 70, 2),
+       ("Noelle", "Terra", 150, 120, 4),
+       ("Kaeya", "Gelo", 110, 60, 4),
+	   ("Qiqi", "Gelo", 80, 100, 5),
+       ("Jean", "Vento", 90, 1800, 4);
+
+SELECT * FROM tb_personagens;
+
+SELECT * FROM tb_personagens WHERE ataque > 2000;
+
+SELECT * FROM tb_personagens WHERE defesa BETWEEN 1000 AND 2000;
+
+SELECT * FROM tb_personagens WHERE nome LIKE "%C%";
+
+SELECT nome, elemento, ataque, defesa, nome_classe, arma 
+FROM tb_personagens INNER JOIN tb_classes
+ON tb_personagens.classes_id = tb_classes.id;
+
+SELECT nome, elemento, ataque, defesa, nome_classe, arma
+FROM tb_personagens INNER JOIN tb_classes
+ON tb_personagens.classes_id = tb_classes.id
+WHERE tb_classes.nome_classe = "Lanceiro";
+
---
+++ Exercicios Dia 2/Ex02.sql
@@ -0,0 +1,53 @@
+CREATE DATABASE db_pizzaria_legal;
+
+USE db_pizzaria_legal;
+
+CREATE TABLE tb_categorias(
+id BIGINT AUTO_INCREMENT PRIMARY KEY,
+tipo VARCHAR(50) NOT NULL,
+descrição VARCHAR(50) NOT NULL
+);
+
+INSERT INTO tb_categorias(tipo, descrição)
+VALUES ("Salgada", "Tradicional"),
+       ("Salgada", "Premium"),
+       ("Salgada", "Vegana"),
+       ("Doce", "Tradicional"),
+       ("Doce", "Premium");
+
+CREATE TABLE tb_pizzas(
+id BIGINT AUTO_INCREMENT PRIMARY KEY,
+sabor VARCHAR(100) NOT NULL,
+massa VARCHAR(50) NOT NULL, 
+tamanho VARCHAR(50) NOT NULL,
+preco DECIMAL(10,2) NOT NULL,
+categorias_id BIGINT,
+FOREIGN KEY (categorias_id) REFERENCES tb_categorias(id)
+);
+
+INSERT INTO tb_pizzas(sabor, massa, tamanho, preco, categorias_id)
+VALUES ("Calabresa", "Padrão", "Grande", 45.90, 1),
+       ("Brigadeiro", "Padrão", "Média", 38.50, 4),
+       ("Margherita", "Padrão", "Pequena", 30.00, 1),
+       ("Quatro Queijos", "Pan", "Família", 72.00, 2),
+	   ("Frango com Catupiry", "Padrão", "Média", 42.00, 1),
+       ("Nutella com Morango", "Pan", "Broto", 39.00, 5),
+       ("Brocolis", "Padrão", "Pequena", 32.50, 3),
+       ("Pepperoni com Cream Cheese", "Padrão", "Grande", 69.90, 2);
+       
+SELECT * FROM tb_pizzas;
+
+SELECT * FROM tb_pizzas WHERE preco > 45.00;
+
+SELECT * FROM tb_pizzas WHERE preco BETWEEN 50.00 AND 100.00;
+
+SELECT * FROM tb_pizzas WHERE sabor LIKE "%M%";
+
+SELECT sabor, massa, tamanho, preco, tipo, descrição 
+FROM tb_pizzas INNER JOIN tb_categorias
+ON tb_pizzas.categorias_id = tb_categorias.id;
+
+SELECT sabor, massa, tamanho, preco, tipo, descrição 
+FROM tb_pizzas INNER JOIN tb_categorias
+ON tb_pizzas.categorias_id = tb_categorias.id
+WHERE tb_categorias.tipo = "Doce";
